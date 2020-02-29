6f4afcf37c74d4f776d463dd4f4f749349517ea3
zhoushunjin <46296948+zhoushunjin@users.noreply.github.com>
2020-02-29 01:52:30 -0600

    Add files via upload

--- word转pdf.py
+++ word转pdf.py
@@ -6,6 +6,8 @@ import os
 import tkinter
 from tkinter import filedialog
 import msvcrt
+import threading
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 def createPdf(wordPath, pdfPath):
@@ -23,22 +25,33 @@ def createPdf(wordPath, pdfPath):
     word.Quit(constants.wdDoNotSaveChanges)
 
 def start(filename):
-    print(filename)
     (filepath, tempfilename) = os.path.split(filename)
     (pdfPath1, extension) = os.path.splitext(tempfilename)
     pdfPath=filepath+"\\"+pdfPath1+".pdf"
     createPdf(filename,pdfPath)
+    print(filename,end="  转换为  ")
     print(pdfPath)
     print()
 
 
+def main():
+    root = tkinter.Tk()    # 创建一个Tkinter.Tk()实例
+    root.withdraw()       # 将Tkinter.Tk()实例隐藏
+    default_dir = r"文件路径"
+    file_path = tkinter.filedialog.askopenfilenames(title=u'选择文件', initialdir=(os.path.expanduser(default_dir)),filetypes=[('Word', '*.doc *.docx')])
+    filename=[]
+    for i in range(len(file_path)):
+        name=file_path[i].replace('/','\\')
+        filename.append(name)
+    print('开始转换,一共%s个文档' %(len(filename)))
+    print()
+    pool = ThreadPool()
+    pool.map(start,filename)
+    pool.close()
+    pool.join()
+    print('转换完成')
+    print()
+    input('按任意键退出')
 
-root = tkinter.Tk()    # 创建一个Tkinter.Tk()实例
-root.withdraw()       # 将Tkinter.Tk()实例隐藏
-default_dir = r"文件路径"
-file_path = tkinter.filedialog.askopenfilenames(title=u'选择文件', initialdir=(os.path.expanduser(default_dir)),filetypes=[('Word', '*.doc *.docx')])
-#file_path=file_path.replace('/','\\')
-for i in range(len(file_path)):
-    filename=file_path[i].replace('/','\\')
-    start(filename)
-input('按任意键退出')
+if __name__ == '__main__':
+    main()
